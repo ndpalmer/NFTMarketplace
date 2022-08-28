dd9a4fc9af06361cfe6c62d40ffb7dd61b47c686
ndpalmer <ndpalmer@gmail.com>
2022-08-29 08:10:54 +1000

    Initial commit.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ['ipfs.infura.io'],
+  },
+  env: {
+    projectId: '2DrchQRnNLAZrkdScfmMm7Q45li',
+    projectSecret: 'bd3b9450feee1a15c9ca6c5ecdc34b17',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
+import { Navbar, Footer } from '../components';
+import { NFTProvider } from '../context/NFTContext';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => (
+  <NFTProvider>
+    <ThemeProvider attribute="class">
+      <div className="dark:bg-nft-black-2 bg-white min-h-screen">
+        <Navbar />
+        <div className="pt-65">
+          <Component {...pageProps} />
+        </div>
+        <Footer />
+      </div>
 
-export default MyApp
+      <Script src="https://kit.fontawesome.com/a39c3493f0.js" crossOrigin="anonymous" />
+    </ThemeProvider>
+  </NFTProvider>
+);
+export default MyApp;
